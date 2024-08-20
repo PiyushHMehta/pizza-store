81335c6214bb3138a2b376a77962974f38bcb6d2
PiyushHMehta <jainpiyush1450@gmail.com>
2024-08-20 19:59:37 +0530

    changing env vars name for aws

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,9 +1,9 @@
 
-import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import uniqid from 'uniqid';
 
 export async function POST(req) {
-  const data =  await req.formData();
+  const data = await req.formData();
   if (data.get('file')) {
     // upload the file
     const file = data.get('file');
@@ -11,8 +11,8 @@ export async function POST(req) {
     const s3Client = new S3Client({
       region: 'ap-south-1',
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
       },
     });
 
@@ -35,7 +35,7 @@ export async function POST(req) {
     }));
 
 
-    const link = 'https://'+bucket+'.s3.amazonaws.com/'+newFileName;
+    const link = 'https://' + bucket + '.s3.amazonaws.com/' + newFileName;
     return Response.json(link);
   }
   return Response.json(true);
