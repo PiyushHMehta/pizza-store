d5ff0e5713deafb753d192f048b119a6a24f7e21
PiyushHMehta <jainpiyush1450@gmail.com>
2024-08-23 21:00:08 +0530

    favicon changes 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,18 +19,18 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <Head>
         <title>{metadata.title}</title>
-        <link rel="icon" href="/public/pizza.png" type="image/png" />
+        <link rel="icon" href="/pizza.png" type="image/png" />
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/public/pizza.png" />
+        <meta property="og:image" content="/pizza.png" />
         <meta property="og:url" content="https://my-pizzza-store.vercel.app/" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="/public/pizza.png" />
+        <meta name="twitter:image" content="/pizza.png" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
